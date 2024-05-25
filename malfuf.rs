5fae33fa3439f2379077aaf708cc6b0d48631946
Faddy Michel Samaan <faddy.michel@gmail.com>
2024-05-25 17:43:34 -0400

    @faddymichel: On branch master
    *	new file:   malfuf.rs
    *	deleted:    maqsum.rhy
    *	new file:   maqsum.rs

---
+++ malfuf.rs
@@ -0,0 +1,14 @@
+?# cat - > .malfuf.scratch
+
++==
+
+length 2
+measure 4
+
+0/bd
+1.5/sn
+3/sn
+
+-==
+
+?# roll index.rs .malfuf.scratch
---
+++ maqsum.rs
@@ -0,0 +1,45 @@
+?# cat - > .maqsum.scratch
+
++==
+
+measure 32
+
+0/clubkick/13
+
+2/e/63
+
+4/realclaps/17
+
+6/realclaps/33
+6/e/63
+
+8/e/63
+
+10/e/63
+
+12/realclaps/17
+12/linnhats/33
+
+14/e/63
+
+16/clubkick/23
+
+18/e/63
+
+20/e/63
+
+22/e/63
+
+24/realclaps/17
+24/linnhats/33
+
+26/e/63
+
+28/e/63
+
+30/realclaps/33
+30/e/63
+
+-==
+
+?# roll index.rs .maqsum.scratch
